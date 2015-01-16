f15298b7891149e5da2be5d45364742e828200c7
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-16 01:42:16 +0000

    Update code to be more in line with C style of building TCP sockets.

--- tcp_client.rb
+++ tcp_client.rb
@@ -1,8 +1,18 @@
 require 'socket'
 
-connection = TCPSocket.open('localhost', 2000)
+client_socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP)
+client_socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+socket_address = Socket.pack_sockaddr_in(53000, '127.0.0.1')
+client_socket.bind(socket_address)
 
-while line = connection.gets
-  puts line
+server_address = Socket.pack_sockaddr_in(2000, '127.0.0.1')
+client_socket.connect(server_address)
+
+while line = client_socket.gets
+  puts "#{line}\n"
 end
-connection.close
+
+client_socket.close
+
+
+
--- tcp_server.rb
+++ tcp_server.rb
@@ -1,12 +1,18 @@
 require 'socket'
 
 server = TCPServer.open('localhost', 2000)
+server_socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP)
+server_socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+socket_address = Socket.pack_sockaddr_in(2000, '127.0.0.1')
+server_socket.bind(socket_address)
+server_socket.listen(5)
+
 loop do
-  connection = server.accept
+  new_socket, _ = server_socket.accept
   puts "Opening a connection upon request."
-  connection.write("It is now #{Time.now.ctime}")
-  connection.write "Bye!"
-  connection.write "#{connection.peeraddr}"
-  connection.close
+  puts "Connected to #{new_socket.remote_address.ip_address}:#{new_socket.remote_address.ip_port}"
+  new_socket.write "It is now #{Time.now.ctime}\r\n"
+  new_socket.write "Bye!"
+  new_socket.close
   puts "Closing connection."
 end
