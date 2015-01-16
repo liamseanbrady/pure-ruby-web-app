e7e618f8391e1640634f1195eb65dd79cec6f86d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-16 02:30:55 +0000

    Hit localhost port 2000 instead of Google's servers. Hit from port 12000 on localhost.

--- http_client.rb
+++ http_client.rb
@@ -1,10 +1,10 @@
 require 'socket'
 
 client_socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, Socket::IPPROTO_TCP)
-client_address = Socket.pack_sockaddr_in(2000, '192.168.0.87')
+client_address = Socket.pack_sockaddr_in(12000, '192.168.0.87')
 client_socket.bind(client_address)
 
-server_address = Socket.pack_sockaddr_in(80, 'www.google.co.uk')
+server_address = Socket.pack_sockaddr_in(2000, '127.0.0.1')
 client_socket.connect(server_address)
 
 client_socket.puts "GET / HTTP/1.1\r\n"
