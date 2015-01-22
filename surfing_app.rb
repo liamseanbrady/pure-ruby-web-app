2e0aa2e21e372e159a701c15d8d0e73ea8ae7465
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-22 04:08:03 +0000

    Remove parsing of content-type. Middleware now takes care of this.

--- surfing_app.rb
+++ surfing_app.rb
@@ -1,15 +1,10 @@
 class Surfing
   def call(env)
+    puts "Calling the app !!!!!!!!!!!!!!"
     document_name = env[:path].slice(/\/(.*)/)
-    suffix = document_name.slice(/\.(.*)/)
-    content_type = case suffix
-                  when ".html" then 'text/html'
-                  when ".css" then 'text/css'
-                  when '.jpg' then 'image/jpeg'
-                  end
     document_path = "documents#{document_name}"
     if File.exists?(document_path)
-      [200, {'Content-Type' => content_type}, [File.read(document_path)]]
+      [200, {}, [File.read(document_path)]]
     else
       [404, {}, ["<html><body><h1>Can't find the page you requested </h1></body></html>"]]
     end
