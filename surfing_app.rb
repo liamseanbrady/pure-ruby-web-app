834101ec30c6bb776c20cf8a111b6d77220a3c6b
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-28 02:14:57 +0000

    Refactor.

--- surfing_app.rb
+++ surfing_app.rb
@@ -1,37 +1,50 @@
-require 'pry'
 require 'mustache'
 
 class Surfing
-  DATA = {
-    "journal" => [
-      {'title' => %q{A Look Back: "Huge Waves!"},
-      'body' => %q{"Today's waves are record setting! 10 foot waves coming from the south and heading to the north. Hide your kids, hide your wife. Don't come out without your board!" A write up about the record setting day of surfing that went down in the history books. Don't miss this recount of the biggest surfing day in Carmel, CA.}},
-      {'title' => %q{Feature Article: Johnny Smith},
-      'body' => %q{Listen to local professional surfer, Johnny Smith, talk about the ups and downs of having a career in professional surfing. Johnny shares insight into his surfing philosophy, how he ended up where he is, and where he's planning on going.}},
-      {'title' => %q{Tips: How to Catch the Waves You Want},
-      'body' => %q{Local surfing instructor shares his secrets to watching the ocean and catching the waves that are worth your effort. Being choosy is an advantage with this strategy and will ensure a fun and fulfilling day of surfing.}},
-      {'title' => %q{Feature Article: Attacked by a Shark and Still Surfing},
-      'body' => %q{Our own hometown hero shares the terrifying story about a nearly fatal shark attack that took an arm but only fanned the fire of passion for a surfer's craft. This article shows us that even in the face of adversity, there is much to live for and much to achieve.}}
-    ]
-  }
+  def initialize
+    @journal_entries = journal_entries
+  end
 
   def call(env)
     request = Rack::Request.new(env)
-    name_hash = request.params
-    DATA.merge!(name_hash) if !name_hash.nil?
-    DATA['journal'].map do |j| 
-      j['body'] = j['body'].slice(0..199)
-      j
-    end
-    if env['PATH_INFO'] == '/'
-      env['PATH_INFO'] = '/index.html'
-      env['REQUEST_PATH'] = '/index.html'
-      template = File.read('templates/index_truncated.mustache')
-      body = Mustache.render(template, DATA)
-      [302, {}, [body]]
+    if request.get? && request.path == '/'
+      data = { 'journal_entries' => journal_entries_truncated}
+      content = Mustache.render(File.read("templates/index_truncated.mustache"), data)
+      [200, {}, [content]]
     else
       Rack::File.new('documents').call(env)
     end
+  end
+
+  def journal_entries_truncated
+    @journal_entries.map do |entry|
+      if entry[:content].size > 200
+        content = entry[:content].slice(0..199) + " ..."
+      else
+        content = entry[:content]
+      end
+      {title: entry[:title], content: content}
+    end
+  end
+
+  def journal_entries
+    [{title: 'A Look Back: Huge Waves!',
+      content: <<-CONTENT
+      Today's waves are record setting! 10 foot waves coming from the south and heading to the north. Hide your kids, hide your wife. Don't come out without your board! It's a record setting day of surfing that went down in the history books. Don't miss this recount of the biggest surfing day in Carmel, CA.
+      CONTENT
+    },{title: 'Feature Article: Johnny Smith',
+       content: <<-CONTENT
+       Listen to local professional surfer, Johnny Smith, talk about the ups and downs of having a career in professional surfing. Johnny shares insight into his surfing philosophy, how he ended up where he is, and where he's planning on going.
+       CONTENT
+    },{title: 'Tips: How to Catch the Waves You Want',
+       content: <<-CONTENT
+       Local surfing instructor shares his secrets to watching the ocean and catching the waves that are worth your effort. Being choosy is an advantage with this strategy and will ensure a fun and fulfilling day of surfing.
+       CONTENT
+    },{title: 'Feature Article: Attacked by a Shark and Still Surfing',
+       content: <<-CONTENT
+       Our own hometown hero shares the terrifying story about a nearly fatal shark attack that took an arm but only fanned the fire of passion for a surfer's craft. This article shows us that even in the face of adversity, there is much to live for and much to achieve.
+       CONTENT
+    }]
   end
 end
 
