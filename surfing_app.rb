26d9c7d0f5bad7f2573ccefaa2084d275ff3df5d
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-28 02:18:14 +0000

    Add id key to each entry in the journal_entries array of hashes.

--- surfing_app.rb
+++ surfing_app.rb
@@ -28,20 +28,28 @@ class Surfing
   end
 
   def journal_entries
-    [{title: 'A Look Back: Huge Waves!',
+    [{
+      id: 1,
+      title: 'A Look Back: Huge Waves!',
       content: <<-CONTENT
       Today's waves are record setting! 10 foot waves coming from the south and heading to the north. Hide your kids, hide your wife. Don't come out without your board! It's a record setting day of surfing that went down in the history books. Don't miss this recount of the biggest surfing day in Carmel, CA.
       CONTENT
-    },{title: 'Feature Article: Johnny Smith',
-       content: <<-CONTENT
+    },{
+      id: 2,
+      title: 'Feature Article: Johnny Smith',
+      content: <<-CONTENT
        Listen to local professional surfer, Johnny Smith, talk about the ups and downs of having a career in professional surfing. Johnny shares insight into his surfing philosophy, how he ended up where he is, and where he's planning on going.
        CONTENT
-    },{title: 'Tips: How to Catch the Waves You Want',
-       content: <<-CONTENT
+    },{
+      id: 3,
+      title: 'Tips: How to Catch the Waves You Want',
+      content: <<-CONTENT
        Local surfing instructor shares his secrets to watching the ocean and catching the waves that are worth your effort. Being choosy is an advantage with this strategy and will ensure a fun and fulfilling day of surfing.
        CONTENT
-    },{title: 'Feature Article: Attacked by a Shark and Still Surfing',
-       content: <<-CONTENT
+    },{
+      id: 4,
+      title: 'Feature Article: Attacked by a Shark and Still Surfing',
+      content: <<-CONTENT
        Our own hometown hero shares the terrifying story about a nearly fatal shark attack that took an arm but only fanned the fire of passion for a surfer's craft. This article shows us that even in the face of adversity, there is much to live for and much to achieve.
        CONTENT
     }]
