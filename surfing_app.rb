b7b0b02ddc4414a7a7a486e4181a05b845393708
Liam Sean Brady <liamseanbrady@gmail.com>
2015-01-28 00:46:43 +0000

    Add rendering of mustache template if path is index. Store data in array of hashes for journals.

--- surfing_app.rb
+++ surfing_app.rb
@@ -1,12 +1,24 @@
 require 'rack'
 require 'pry'
+require 'mustache'
 
 class Surfing
+  DATA = {
+    "journal" => [
+      {'title' => 'Huge Waves',
+      'body' => "Today's waves are big."},
+      {'title' => 'Massive Waves',
+      'body' => "Today's waves are massive."}
+    ]
+  }
+
   def call(env)
     if env['PATH_INFO'] == '/'
       env['PATH_INFO'] = '/index.html'
       env['REQUEST_PATH'] = '/index.html'
-      [302, {} , [File.read('documents/index.html')]]
+      template = File.read('templates/index.mustache')
+      body = Mustache.render(template, DATA)
+      [302, {} , [body]]
     else
       Rack::File.new('documents').call(env)
     end
