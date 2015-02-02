8ae93a0dcd630cd0bc3b3b102ee8565e3c5467d6
Liam Sean Brady <liamseanbrady@gmail.com>
2015-02-02 19:15:55 +0000

    Save data to ivar instead of passing into render as param. Make param symbol. Access keys in hashes as strings.

--- surfing_app.rb
+++ surfing_app.rb
@@ -1,19 +1,24 @@
 require 'mustache'
-require 'yaml'
+require 'sqlite3'
+require 'pry'
 
 class Surfing
   def initialize
-    persisted_data = File.open('data/journal_entries.yaml') { |f| f.read }
-    @journal_entries = YAML.load(persisted_data)
+    @db = SQLite3::Database.new "data/surfing.sqlite3"
+    @db.results_as_hash = true
   end
 
   def call(env)
     request = Rack::Request.new(env)
     if request.get? && request.path == '/'
-      render("index_truncated", {'journal_entries' => journal_entries_truncated})
+      @journal_entries = @db.execute "SELECT * FROM journal_entries"
+      binding.pry
+      @data = {'journal_entries' => journal_entries_truncated}
+      render :index_truncated
     elsif request.get? && request.path == '/show_journal_entry'
-      entry =  @journal_entries.select {|entry| entry[:id].to_s == request.params['id']}
-      render("show", {'journal_entry' => entry})
+      entry =  @journal_entries.select {|entry| entry['id'].to_s == request.params['id']}
+      @data = {'journal_entry' => entry}
+      render :show
     elsif request.post? && request.path == '/create_journal_entry'
       new_entry = {id: @journal_entries.size + 1, title: request.params['title'], content: request.params['content']}
       @journal_entries.unshift(new_entry)
@@ -23,8 +28,8 @@ class Surfing
     end
   end
 
-  def render(template_name, data)
-    content = Mustache.render(File.read("templates/#{template_name}.mustache"), data)
+  def render(template_name)
+    content = Mustache.render(File.read("templates/#{template_name}.mustache"), @data)
     [200, {}, [content]]
   end
 
@@ -34,12 +39,12 @@ class Surfing
 
   def journal_entries_truncated
     @journal_entries.map do |entry|
-      if entry[:content].size > 200
-        content = entry[:content].slice(0..199) + " ..."
+      if entry['content'].size > 200
+        content = entry['content'].slice(0..199) + " ..."
       else
-        content = entry[:content]
+        content = entry['content']
       end
-      {id: entry[:id], title: entry[:title], content: content}
+      {id: entry['id'], title: entry['title'], content: content}
     end
   end
 
